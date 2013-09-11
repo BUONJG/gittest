547e929f95dcc9c5430466159e7704331aa70dfe
RenaudGB <renaud.guiot@sopragroup.com>
2013-09-11 17:17:58 +0200

    Page de test RGB

--- test2.php
+++ test2.php
@@ -1,5 +1,21 @@
 <?php
+include "../../include/include.html.php";
 
-echo "test";
+ggipsi()->settitle('Test');
 
+//Ici mon test
+
+$iQuery = gdb('ips_ps','t_achats_recep')->query('select');
+$iQuery->select('DATE_RECEP_J');
+$iQuery->where("RLNRCP = '101033235'");
+$tableau = $iQuery->gethtbl();
+
+$tableau->display();
+
+echo nhtbl($tableau->getvalue(0))->getvalue('DATE_RECEP_J');
+echo nhtbl($tableau->getvalue(1))->getvalue('DATE_RECEP_J');
+
+////////////////////
+ggipsi()->display();
+////////////////////
 ?>
