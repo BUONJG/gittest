9709f24f7941c8b708314d90ca08331fefa807bd
RenaudGB <renaud.guiot@sopragroup.com>
2013-09-12 11:59:32 +0200

    a

--- test2.php
+++ test2.php
@@ -1,26 +1,4 @@
 <?php
-include "../../include/include.html.php";
-
-<<<<<<< HEAD
-ggipsi()->settitle('Test');
-
-//Ici mon test
-
-$iQuery = gdb('ips_ps','t_achats_recep')->query('select');
-$iQuery->select('DATE_RECEP_J');
-$iQuery->where("RLNRCP = '101033235'");
-$tableau = $iQuery->gethtbl();
-
-$tableau->display();
-
-echo nhtbl($tableau->getvalue(0))->getvalue('DATE_RECEP_J');
-echo nhtbl($tableau->getvalue(1))->getvalue('DATE_RECEP_J');
-
-////////////////////
-ggipsi()->display();
-////////////////////
-=======
-
 
 
 
@@ -29,5 +7,5 @@ echo "test2";
 
 
 echo "test branch Test";
->>>>>>> origin/test
+
 ?>
